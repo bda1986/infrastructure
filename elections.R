613fc961dc18b6edfc4002dd17ec22003b33cca4
randomRuser <66870334+randomRuser@users.noreply.github.com>
2025-04-24 17:53:32 +0200

    Update elections.R

--- elections.R
+++ elections.R
@@ -1,1 +1,28 @@
+#install.packages("gerda")
+
+# Load the package
+library(gerda)
+library(dplyr)
+
+# List available datasets
+available_data <- gerda_data_list()
+
+# Load county dataset
+data_cty_harm <- load_gerda_web("federal_cty_harm", verbose = TRUE, file_format = "rds")
+
+# Describe data
+head(data_cty_harm)
+names(data_cty_harm)
+
+# Create ARS variable by renaming county_code (for matching)
+data_cty_harm <- data_cty_harm %>%
+  rename(ARS = county_code)
+
+# Create a new dataset only including last election
+data_cty_latest <- data_cty_harm %>%
+  group_by(ARS) %>%
+  filter(election_year == max(election_year, na.rm = TRUE)) %>%
+  ungroup()
+
+
 
