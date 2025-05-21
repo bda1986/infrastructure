79eb1703bc6dfeb58593e90be2cdc2bdf8a7be53
unknown <patrick.hufschmidt@posteo.net>
2025-05-21 19:09:52 +0200

    Update elections script
    Data export federal elections at the municipal level (1990-2021)

--- elections.R
+++ elections.R
@@ -3,27 +3,29 @@
 # Load the package
 library(gerda)
 library(dplyr)
+library(rio)
 
 # List available datasets
 available_data <- gerda_data_list()
 available_data
 
 # Load county dataset
-datam <- load_gerda_web("municipal_harm", verbose = TRUE, file_format = "rds")
+data <- load_gerda_web("federal_muni_harm", verbose = TRUE, file_format = "rds")
 
 # Describe data
 head(data)
 names(data)
 
 # Create ARS variable by renaming county_code (for matching)
-data <- data %>%
-  rename(AGS = county_code)
+#data <- data %>%
+ # rename(AGS = county_code)
 
 # Create a new dataset only including last election
 data_latest <- data %>%
-  group_by(AGS) %>%
+  group_by(ags) %>%
   filter(election_year == max(election_year, na.rm = TRUE)) %>%
   ungroup()
 
+export(data_latest, "F:/git/infrastructure/data/elections/federal_muni_harm.RData")
 
 
