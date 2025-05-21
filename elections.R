f656f33d8a9f681db131fd5324a8a311d404156e
unknown <patrick.hufschmidt@posteo.net>
2025-05-21 18:57:16 +0200

    script update

--- elections.R
+++ elections.R
@@ -9,18 +9,18 @@ available_data <- gerda_data_list()
 available_data
 
 # Load county dataset
-data_cty_harm <- load_gerda_web("federal_cty_harm", verbose = TRUE, file_format = "rds")
+datam <- load_gerda_web("municipal_harm", verbose = TRUE, file_format = "rds")
 
 # Describe data
-head(data_cty_harm)
-names(data_cty_harm)
+head(data)
+names(data)
 
 # Create ARS variable by renaming county_code (for matching)
-data_cty_harm <- data_cty_harm %>%
+data <- data %>%
   rename(AGS = county_code)
 
 # Create a new dataset only including last election
-data_cty_latest <- data_cty_harm %>%
+data_latest <- data %>%
   group_by(AGS) %>%
   filter(election_year == max(election_year, na.rm = TRUE)) %>%
   ungroup()
